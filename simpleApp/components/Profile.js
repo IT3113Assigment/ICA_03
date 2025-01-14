870856235be231cc84148a1da0adbe6ca6095428
chamithjeewantha <chamithjeewa123@gmail.com>
2025-01-14 16:36:34 +0530

    create navigation in app.js

---
+++ simpleApp/components/Profile.js
@@ -0,0 +1,13 @@
+import { Text } from "react-native-paper";
+import { View } from "react-native-web";
+
+export default function Profile() {
+  return (
+    <View>
+      <Text>Name:</Text>
+      <Text>Age:</Text>
+      <Text>Gender:</Text>
+      <Text>Email:</Text>
+    </View>
+  );
+}

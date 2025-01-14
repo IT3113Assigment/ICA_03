b8b01c10b96775eed98d2856b25c01fd0a88d6a0
chamithjeewantha <chamithjeewa123@gmail.com>
2025-01-14 16:25:08 +0530

    create studentList component

---
+++ simpleApp/components/StudentList.js
@@ -0,0 +1,26 @@
+import { FlatList, StyleSheet, View } from "react-native";
+import { Text } from "react-native-paper";
+import { students } from "./StudentDb";
+export default function StudentList() {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={students}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Text styles={styles.item}>{item.name}</Text>}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});

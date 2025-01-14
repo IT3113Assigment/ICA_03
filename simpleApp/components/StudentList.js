9d2d2d05e4366f009098c85ac9ff4fc54729e4f6
chamithjeewantha <chamithjeewa123@gmail.com>
2025-01-14 16:42:42 +0530

    add touchable oppacity button for items

--- simpleApp/components/StudentList.js
+++ simpleApp/components/StudentList.js
@@ -1,13 +1,19 @@
-import { FlatList, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, TouchableOpacity, View } from "react-native";
 import { Text } from "react-native-paper";
 import { students } from "./StudentDb";
+import { useNavigation } from "@react-navigation/native";
 export default function StudentList() {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <FlatList
         data={students}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Text styles={styles.item}>{item.name}</Text>}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
+            <Text styles={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
       />
     </View>
   );
